5211b2ffe7bf2cdc76a6614995a8a6c717e3c9ee
naxtor <nabilfaris65@gmail.com>
2025-04-07 17:11:25 +0700

    Add app constants

---
+++ app/lib/utilities/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  // Route constants
+  static const String searchRoute = '/';
+  static const String profileRoute = '/profile';
+  static const String repositoryDetailsRoute = '/profile/repository-details';
+
+  // API constants
+  static const String githubProfileURL = 'https://api.github.com/users/';
+}
